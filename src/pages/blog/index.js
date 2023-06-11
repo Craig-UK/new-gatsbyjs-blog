92cba99818097c8a4e07008e52653a4ddf3d1863
Craig-UK <craigukdev@gmail.com>
2023-06-12 00:37:41 +0100

    FEAT: Make it so that the 'blog posts' page contains the links to each of the separate 'Blog Post' pages/routes within the title of the Blog Post using the Blog Post's 'slug'.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,36 +1,40 @@
-import React from "react";
-import { graphql } from "gatsby";
+import * as React from "react";
+import { Link, graphql } from "gatsby";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const BlogPage = ({ data }) => {
+  console.log(data.allMdx);
   return (
     <Layout pageTitle="My Blog Posts">
-        {data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
-          </article>
-        ))}
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>
+            <Link to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allMdx(sort: { frontmatter: { date: DESC } }) {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
         frontmatter {
-          title
           date(formatString: "MMMM D, YYYY")
+          title
+          slug
         }
         id
-        excerpt
       }
     }
   }
-`;
+`
 
 export const Head = () => <Seo title="My Blog Posts" />;
 
